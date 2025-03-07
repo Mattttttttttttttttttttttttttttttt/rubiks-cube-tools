7565ea22bb4cc66dbadc38fc441f16e6f4fb2723
Matt <163932726+Mattttttttttttttttttttttttttttttt@users.noreply.github.com>
2025-03-06 18:59:37 -0800

    Update average-stats-calculator.py
    supports solves in the format of current session statistics

--- average-stats-calculator.py
+++ average-stats-calculator.py
@@ -2,273 +2,8 @@
 
 import re
 import statistics
-import sys
-
-
-def find_all(parent: list, daughter: str) -> int:
-    """count how many substrings is present in the parent string
-
-    Args:
-        parent (str/list): the parent string to search in
-        daughter (str): the substring needing to be searched
-
-    Returns:
-        int: the count of how many substrings is present
-    """
-    # parent can be string or list, online python interpreter thinks | is an error
-    count: int = 0
-    if isinstance(parent, list):
-        parent = list(parent)
-        parent = deepjoin(parent, "")
-    # while parent string contains daughter string
-    while parent.find(daughter) != -1:
-        # limit searching scope
-        parent = parent[parent.find(daughter) + len(daughter):]
-        count += 1
-    return count
-
-def deepjoin(lst: list, joiner: str) -> str:
-    """returns a list joined with joiner
-
-    Args:
-        lst (list): the list to be joined
-        joiner (str): the connector between elements
-
-    Returns:
-        str: the resultant string
-    """
-    lst = list(lst)
-    for i, value in enumerate(lst):
-        lst[i] = str(value)
-    return joiner.join(lst)
-
-def num_part(time: str) -> str:
-    """extracts the decimal part of a string
-
-    Args:
-        time (str): string to be extracted
-
-    Returns:
-        str: decimal in string, minutes converted to seconds
-    """
-    return "".join([i for i in list(time) if (i.isdigit() or i == "." or i == ":")])
-
-def no_brackets(time: str) -> str:
-    """get rid of the bracket part in the input string
-
-    Args:
-        time (str): the string to be processed
-
-    Returns:
-        str: the resultant string without the bracket part
-    """
-    return re.compile(r"\[[a-zA-Z0-9 !@#$%^&*()_+-=|\\{};':\"'<>?,./`~]*\]").sub("", time)
-
-def no_paren(time: str) -> str:
-    """get rid of the parentheses part in the input string
-
-    Args:
-        time (str): the string to be processed
-
-    Returns:
-        str: the resultant string without the parentheses part
-    """
-    return re.compile(r"\(|\)").sub("", time)
-
-def valid_num(num: int) -> str:
-    """adds 0 before a one-digit number
-
-    Args:
-        num (int): number to be modified
-
-    Returns:
-        str: the resultant string
-    """
-    return str(num) if num >= 10 else "0" + str(num)
-
-def minutes(a: str) -> float:
-    """used for the min and max function to convert min:sec into seconds
-
-    Args:
-        a (str): the time in a string
-
-    Returns:
-        float: the converted time in seconds
-    """
-    a = num_part(a)
-    if ":" in a:
-        return float(a.split(":")[1]) + 60 * int(a.split(":", maxsplit=1)[0])
-    else:
-        return float(a)
-
-def seconds(a: str) -> str:
-    """converts a potential min:sec in string or integer back to a min:sec string
-
-    Args:
-        a (str/int): the string or integer to be converted (e.g. 60.67)
-
-    Returns:
-        str: a string of min:sec or the original float
-    """
-    # a can be string or int, online python interpreter thinks | is an error
-    if isinstance(a, int):
-        return str(a) if a < 60 else f"{a // 60}:{valid_num(a % 60)}"
-    a = str(a)
-    if a[-1] == "+":
-        a = float(a[:-1])
-        dec: int = len(str(a).split(".")[1])
-        return str(a) + "+" if a < 60 else f"{int(a // 60)}:{valid_num(round(a % 60, dec))}+"
-    else:
-        a = float(a)
-        dec: int = len(str(a).split(".")[1])
-        return str(a) if a < 60 else f"{int(a // 60)}:{valid_num(round(a % 60, dec))}"
-
-def avg(solves: list, num_solves: int, decimals: int) -> float:
-    """returns ao5
-
-    Args:
-        solves (list): solves
-        num_solves (int): the length of the average
-        decimals (int): the amount of decimals
-
-    Returns:
-        float/str: average value
-    """
-    # return value can be float or string, online python interpreter thinks | is an error
-    assert num_solves >= 3, "you cannot have an average with less than 3 solves"
-    solves = keep(solves, ndnf)
-    solves = [str(i) for i in solves]
-    if len(solves) < num_solves - 1:
-        return "DNF"
-    elif len(solves) == num_solves - 1:
-        solves.remove(min(solves, key=minutes))
-        solves = [float(i) for i in solves]
-        return round(sum(solves) / (num_solves - 2), decimals)
-    else:
-        solves.remove(min(solves, key=minutes))
-        solves.remove(max(solves, key=minutes))
-        solves = [float(i) for i in solves]
-        return round(sum(solves) / (num_solves - 2), decimals)
-
-def avg_compare(time: float) -> float:
-    """compares averages
-
-    Args:
-        time (str/float): the avg
-
-    Returns:
-        float: the interpretation
-    """
-    # time can be string or float, online python interpreter thinks | is an error
-    return sys.maxsize if time == "DNF" else time
-
-def frwrd(lst: list, start: int, value: int) -> list:
-    """returns a list of *value* values frwrd in *lst* starting at *start* index
-
-    Args:
-        lst (list): list to be processed
-        start (int): starting index
-        value (int): number of values to go
-
-    Returns:
-        list: processed list
-    """
-    result = []
-    for i in range(value):
-        result.append(lst[start + i])
-    return result
-
-def repeat(lst: list) -> dict:
-    """checks for any repeat in the list
-
-    Args:
-        lst (list): the list to be processed
-
-    Returns:
-        dict: a dictionary of repeat to the number of times it appeared
-    """
-    lst = sorted(keep(list(lst), ndnf))
-    result: dict = {}
-    count = 0
-    for i, value in enumerate(lst):
-        if i == len(lst) - 2:
-            break
-        if count != 0:
-            count -= 1
-            continue
-        k = i
-        while value == lst[k+1]:
-            count += 1
-            k += 1
-        if count == 0:
-            continue
-        result[value] = count + 1
-    return result
-
-def keep(thing: list, funct) -> list:
-    """keep the elements of thing that satisfy funct
-
-    Args:
-        thing (str/list): thing to filter from
-        funct (function): filter function
-        
-    Returns:
-        str/list: filtered thing
-    """
-    # thing can be string or list, online python interpreter thinks | is an error
-    # output value can be string or list, online python interpreter thinks | is an error
-    thing = list(thing)
-    every = []
-    for i in thing:
-        if not funct(i):
-            every.append(i)
-    for i in every:
-        thing.remove(i)
-    return thing
-
-def prths(a: str)-> bool:
-    """used for the keep function to keep all solves with parentheses
-
-    Args:
-        a (str): the string to be analyzed
-
-    Returns:
-        bool: whether a starts with a parenthese
-    """
-    return a[0] == "("
-
-def nprths(a: str)-> bool:
-    """used for the keep function to keep all solves without parentheses
-
-    Args:
-        a (str): the string to be analyzed
-
-    Returns:
-        bool: whether a starts without a parenthese
-    """
-    return not a[0] == "("
-
-def ndnf(a)-> bool:
-    """filters dnfs
-
-    Args:
-        a (str | any): the solve
-
-    Returns:
-        bool: whether it's a dnf
-    """
-    return not "DNF" in a if isinstance(a, str) else True
-
-def number(a)-> bool:
-    """determine if the input is a float
-
-    Args:
-        a (Any): the thing to be analyzed
-
-    Returns:
-        bool: whether a is a float
-    """
-    return isinstance(a, float)
+from helpers import no_brackets, keep, ndnf, prths, minutes, num_part, seconds, no_paren, frwrd
+from helpers import avg_compare, find_all, deepjoin, repeat, nprths, number, avg
 
 # HELP
 HELP = {"length": "this is the number on the second line of your cstimer average.\n"\
@@ -320,29 +55,32 @@ def check(var: str, inquiry: str, cond, accept_empty: bool=False):
     Returns:
         _type_: the value to be assigned to the variable
     """
-    if isinstance(cond, list):
-        result = input(inquiry)
-        while result not in cond and not (accept_empty and result == ""):
-            if result == "help":
-                print(HELP[var])
-                result = input(inquiry)
-            else:
-                result = input("don't think that's what I asked for, try again: ")
-    else: #cond is a function
-        a = True
-        result = input(inquiry)
-        while a:
-            if not (accept_empty and result == ""):
-                break
-            try:
+    try:
+        if isinstance(cond, list):
+            result = input(inquiry)
+            while result not in cond and not (accept_empty and result == ""):
                 if result == "help":
                     print(HELP[var])
                     result = input(inquiry)
+                else:
+                    result = input("don't think that's what I asked for, try again: ")
+        else: #cond is a function
+            a = True
+            result = input(inquiry)
+            while a:
+                if not (accept_empty and result == ""):
                     break
-                cond(result)
-                a = False
-            except ValueError:
-                result = input("don't think that's what I asked for, try again: ")
+                try:
+                    if result == "help":
+                        print(HELP[var])
+                        result = input(inquiry)
+                        break
+                    cond(result)
+                    a = False
+                except ValueError:
+                    result = input("don't think that's what I asked for, try again: ")
+    except KeyboardInterrupt:
+        exit()
     return result
 
 def integer(num) -> int:
@@ -358,19 +96,18 @@ print("hi there!\nthis program only supports cstimer\n"\
     "type \"help\" at any point to get help on the question!\n")
 # get DATA
 u = []
-print("your average of ... (paste it here and type \"done\" on a new line): ")
+print("your average of ... (paste it in here): ")
 try:
     for line in iter(input, "done"):
         u.append(line)
 except KeyboardInterrupt:
     exit()
 ao: str = "".join(u).strip()
-time_list: list = no_brackets(ao.split("Time List:")[1])
-time_list = [no_brackets(i) for i in time_list.split(", ")]
+time_list: list[str] = no_brackets(ao.split("Time List:")[1]).split(", ")
 LENGTH: int = len(time_list)
-parentheses: list = [re.compile(r"\(|\)").sub("", i)
+parentheses: list[str] = [re.compile(r"\(|\)").sub("", i)
                           for i in keep(keep(time_list, ndnf), prths)]
-r: list = []  #refined list
+r: list[float] = []  #refined list
 for j in time_list:
     if ndnf(j):
         r.append(minutes(num_part(j)))
@@ -430,7 +167,7 @@ repeats = {}
 
 
 # p1
-p1["**best single**:"] = min(parentheses, key=minutes)
+p1["**best single**:"] = min(time_list, key=minutes)
 p1["**worst single**:"] = no_paren(time_list[r.index(max(keep(r, ndnf)))])
 #                                                   ^doesn't need minutes since r is list[float]
 p1["**best counting**:"] = min(keep(keep(time_list, nprths), ndnf), key=minutes)
