31e26ef0864068a41a5434b47a303acda618d40d
Matt <163932726+Mattttttttttttttttttttttttttttttt@users.noreply.github.com>
2025-08-09 15:01:17 -0700

    Update average-generator.py

--- average-generator.py
+++ average-generator.py
@@ -14,7 +14,7 @@ def minutes_dnf(a: str) -> float | str:
         return a
     a = num_part(a)
     if ":" in a:
-        return float(a.split(":")[1]) + 60 * int(a.split(":", maxsplit=1)[0])
+        return round(float(a.split(":")[1]) + 60 * int(a.split(":", maxsplit=1)[0]), len(a.split(".")[1]))
     else:
         return float(a)
 
@@ -43,6 +43,34 @@ def dnf_solve(lst: list) -> None:
         lst[-1] = "DNF(" + seconds(minutes(num_part(lst[-1])) - 2) + ")"
     else:
         lst[-1] = "DNF(" + lst[-1] + ")"
+
+# def avg(solves: list, num_solves: int, delete: int) -> float | str:
+#     """returns average of num_solves
+
+#     Args:
+#         solves (list): solves (cannot be DNF average)
+#         num_solves (int): the length of the average
+#         delete (int): how many solves to trim
+
+#     Returns:
+#         float/str: average value
+#     """
+#     assert num_solves > 2, "you cannot have an average with less than 3 solves"
+#     if num_solves == 3: #calculate mean
+#         return round(sum(solves) / num_solves, 3)
+#     # calculates average
+#     solves = [str(i) for i in solves]
+#     if len(keep(solves, ndnf)) >= num_solves - delete:
+#         for i in range(delete):
+#             trim(solves)
+#         solves = [float(i) for i in solves]
+#         return round(sum(solves) / (num_solves - 2 * delete), 3)
+#     else:
+#         # for i in range(delete):
+#         #     trim(solves)
+#         # solves = [float(i) for i in solves]
+#         # return round(sum(solves) / (num_solves - 2 * delete), 3)
+#         return "DNF"
 
 def add_parenthese(copy: list, solves: list) -> list:
     """adds parentheses around the slowest and fastest solve once
@@ -79,7 +107,7 @@ def avg_str(num: int, solves: list) -> str:
                 solves[fastest_index] = "**" + solves[fastest_index] + "**"
             return "DNF = " + ", ".join(solves)
         else: #no dnf
-            avg_val = round_decimal(solves, seconds(avg([minutes(i) for i in solves], num)))
+            avg_val = round_decimal(solves, seconds(avg([str(minutes(i)) for i in solves], num)))
             fastest_index = solves.index(min(solves, key=minutes))
             solves[fastest_index] = "**" + solves[fastest_index] + "**"
             return avg_val + " = " + ", ".join(solves)
@@ -111,14 +139,12 @@ while length > 2:
             command = input()
             if command == "+":
                 plus_two_solve(average)
-                continue
             elif command == "d":
                 dnf_solve(average)
-                continue
             elif command == "e":
                 del average[-1]
-                continue
-            average.append(command)
+            else:
+                average.append(command)
         print(avg_str(length, average))
     except KeyboardInterrupt:
         pass
