dd8346e708200214ae3879395e6152001195dd4a
Matt <163932726+Mattttttttttttttttttttttttttttttt@users.noreply.github.com>
2025-11-02 20:08:21 -0800

    auto rounding

--- time_addition.py
+++ time_addition.py
@@ -4,9 +4,10 @@
 from helpers import minutes, seconds
 
 # code
+print("enter twice when done.")
 data: list[str] = []
 data.append(input())
 while data[-1] != "":
     data.append(input())
 del data[-1]
-print(seconds(round(sum([minutes(i) for i in data]), 2)))
+print(seconds(round(sum([minutes(i) for i in data]), max([len(i.split(".")[1]) for i in data]))))
