10bcb96767913ce6e749869fd18cbe6b6ac12732
Matt <163932726+Mattttttttttttttttttttttttttttttt@users.noreply.github.com>
2025-11-03 20:31:02 -0800

    allows choosing where to y2, allows to not normalize, bug fix

--- sq1normalizer.py
+++ sq1normalizer.py
@@ -82,18 +82,20 @@ def comma(a: str) -> str:
     inx = sep_index(a)
     return ",".join([a[0:inx], a[inx:]])
 
-def normalize(alg: str, l_fing: bool = False, k = None) -> str:
+def normalize(alg: str, l_f_lst: list[int], k = None, leave: bool = True) -> str:
     """normalizes the alg
 
     Args:
         alg (str): alg to be normalized
-        l_fing (bool): whether we are y2ing the alg, defaulted to no
+        l_f_lst (list[int]): a list of all the positions to y2 at (e.g. [1] for y2 the whole alg)
         k (bool | None): whether to be in karn, defaulted to follow alg
+        leave (bool): whether to leave the alg alone and not normalize
 
     Returns:
         str: normalized alg
     """
     alg = re.sub(r"\[.*\]", "", alg).strip()
+    l_f_lst = [-1] if not l_f_lst else l_f_lst
     k_i = ob_karn(alg) #whether the scram is in karn
     k = ob_karn(alg) if k is None else k # whether to put it in karn
 
@@ -108,25 +110,32 @@ def normalize(alg: str, l_fing: bool = False, k = None) -> str:
         if "/" in list(alg):
             # slash present, can safely replace space
             alg = re.sub(r" ", "", alg) # X space
+            alg = "/".join([f"{legal_move(int(i.split(",")[0]))},{legal_move(int(i.split(",")[1]))}"
+                            for i in alg.split("/")]) # make moves legal
             alg = re.sub(r"\/", " ", alg) # slash to space
             alg = re.sub(r",", "", alg) # X comma
         else:
             # no slash, prob karn in numbers
             alg = re.sub(r" +", " ", alg) # X mutiple spaces
+            alg = " ".join([f"{legal_move(int(i.split(",")[0]))},{legal_move(int(i.split(",")[1]))}"
+                            for i in alg.split(" ")]) # make moves legal TODO
             alg = re.sub(r",", "", alg) # X comma
 
     # now alg in "10 5-1 -51 -10"
-
-    facing_d = l_fing # whether we need to l_f this move
     alst = alg.split(" ")
+    if l_f_lst != [""] and max(l_f_lst) > len(alst)-3:
+        raise ValueError(f"You cannot modify the move after the {max(l_f_lst)}th slice!")
     if alst[0].lower() != "a":
         alst[0] = "A" if changes_alignment(int(alst[0][0:sep_index(alst[0])])) else "a"
         alst[-1] = "A" if changes_alignment(int(alst[-1][0:sep_index(alst[-1])])) else "a"
+
     # now alg in "A 5-1 -51 A"
+    l_fing = 1 in l_f_lst
+    facing_d = l_fing # whether we need to l_f this move
     for i in range(1, len(alst) -2): # avoid checking the last actual move also
         m = alst[i]
         m = l_f(m) if facing_d else m
-        if m not in GOOD:
+        if (not leave and m not in GOOD) or i in l_f_lst:
             m = compl(m)
             l_fing = not l_fing
         facing_d = not facing_d if l_fing else facing_d
@@ -155,11 +164,46 @@ try:
         u.append(line)
 except KeyboardInterrupt:
     exit()
-layer_flip = check("return y2 alg? (yes/no): ",
-             lambda ans: ans.lower().strip() in ["yes", "no"]).lower().strip()
-layer_flipb = True if layer_flip == "yes" else False
-karn = check("in karn? (yes/no, leave empty to follow alg formats): ",
-             lambda ans: ans.lower().strip() in ["yes", "no"],
-             True).lower().strip()
-karnb = True if karn == "yes" else False if karn == "no" else None
-print(*list({normalize(line,layer_flipb,karnb) for line in u}), sep="\n")
+if len(u) > 1:
+    # multiple algs
+    layer_flip = check("return y2 alg? (yes/no): ",
+                lambda ans: ans.lower().strip() in ["yes", "no"]).lower().strip()
+    if layer_flip == "yes":
+        l_f_pos_t = check("where? (leave empty to be at the start, " +
+                        "else indicate after which slices, separated by comma no spaces): ",
+                        lambda ans: all([ch.isdigit() for ch in ans.strip().split(",")]),
+                        True).strip().split(",")
+        l_f_pos = [1] if l_f_pos_t == [""] else [int(ch) for ch in l_f_pos_t] # [2,3], etc.
+    else:
+        l_f_pos = []
+    norm = check("perform normalization? (yes/no, leave empty to be yes): ",
+                lambda ans: ans.lower().strip() in ["yes", "no"], True).lower().strip()
+    normb = False if norm == "no" else True
+    karn = check("in karn? (yes/no, leave empty to follow alg formats): ",
+                lambda ans: ans.lower().strip() in ["yes", "no"],
+                True).lower().strip()
+    karnb = True if karn == "yes" else False if karn == "no" else None
+    print(*list({normalize(line,l_f_pos,karnb,not normb) for line in u}), sep="\n")
+else:
+    # one alg, so we can do a recursion of modification
+    line = u[0]
+    while True:
+        layer_flip = check("return y2 alg? (yes/no): ",
+                    lambda ans: ans.lower().strip() in ["yes", "no"]).lower().strip()
+        if layer_flip == "yes":
+            l_f_pos_t = check("where? (leave empty to be at the start, " +
+                            "else indicate after which slices, separated by comma no spaces): ",
+                            lambda ans: all([ch.isdigit() for ch in ans.strip().split(",")]),
+                            True).strip().split(",")
+            l_f_pos = [1] if l_f_pos_t == [""] else [int(ch) for ch in l_f_pos_t] # [2,3], etc.
+        else:
+            l_f_pos = []
+        norm = check("perform normalization? (yes/no): ",
+                    lambda ans: ans.lower().strip() in ["yes", "no"], True).lower().strip()
+        normb = False if norm == "no" else True
+        karn = check("in karn? (yes/no, leave empty to follow alg formats): ",
+                    lambda ans: ans.lower().strip() in ["yes", "no"],
+                    True).lower().strip()
+        karnb = True if karn == "yes" else False if karn == "no" else None
+        line = normalize(line,l_f_pos,karnb,not normb)
+        print(line)
