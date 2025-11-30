5b3185f74b5061acd72dcf310afbad6fdfb12e8c
Matt <163932726+Mattttttttttttttttttttttttttttttt@users.noreply.github.com>
2025-11-29 21:56:42 -0800

    various updates

--- get_scramble.py
+++ get_scramble.py
@@ -3,7 +3,301 @@
 import random
 import re
 
-OBL = {"1c": "BBwWWwWWwWWw",
+POSSIBLE_OBL = [
+    ["", "solved", "solved"],
+
+    ["", "1c", "1c"],
+    ["", "cadj", "cadj"],
+    ["", "cadj", "copp"],
+    ["", "copp", "copp"],
+    ["", "3c", "3c"],
+    ["", "4e", "4e"],
+    ["", "3e", "3e"],
+    ["", "line", "line"],
+    ["", "L", "line"],
+    ["", "L", "L"],
+    ["", "1e", "1e"],
+    ["good", "pair", "pair"],
+    ["bad", "pair", "pair"],
+    ["good", "arrow", "pair"],
+    ["bad", "arrow", "pair"],
+    ["good", "arrow", "arrow"],
+    ["bad", "arrow", "arrow"],
+    ["", "gem", "gem"],
+    ["", "gem", "knight"],
+    ["", "gem", "axe"],
+    ["", "gem", "squid"],
+    ["good", "knight", "knight"],
+    ["bad", "knight", "knight"],
+    ["good", "knight", "axe"],
+    ["bad", "knight", "axe"],
+    ["same", "axe", "axe"],
+    ["diff", "axe", "axe"],
+    ["", "squid", "knight"],
+    ["", "squid", "axe"],
+    ["", "squid", "squid"],
+    ["good", "thumb", "thumb"],
+    ["bad", "thumb", "thumb"],
+    ["good", "thumb", "bunny"],
+    ["bad", "thumb", "bunny"],
+    ["good", "bunny", "bunny"],
+    ["bad", "bunny", "bunny"],
+    ["", "shell", "shell"],
+    ["", "shell", "bird"],
+    ["", "shell", "hazard"],
+    ["", "yoshi", "shell"],
+    ["good", "bird", "bird"],
+    ["bad", "bird", "bird"],
+    ["", "bird", "hazard"],
+    ["", "hazard", "hazard"],
+    ["good", "yoshi", "bird"],
+    ["bad", "yoshi", "bird"],
+    ["", "yoshi", "hazard"],
+    ["same", "yoshi", "yoshi"],
+    ["diff", "yoshi", "yoshi"],
+    ["good", "kite", "kite"],
+    ["bad", "kite", "kite"],
+    ["good", "kite", "cut"],
+    ["bad", "kite", "cut"],
+    ["", "kite", "T"],
+    ["good", "kite", "N"],
+    ["bad", "kite", "N"],
+    ["", "kite", "tie"],
+    ["", "cut", "T"],
+    ["good", "cut", "N"],
+    ["bad", "cut", "N"],
+    ["", "cut", "tie"],
+    ["good", "cut", "cut"],
+    ["bad", "cut", "cut"],
+    ["good", "T", "T"],
+    ["bad", "T", "T"],
+    ["", "T", "N"],
+    ["good", "T", "tie"],
+    ["bad", "T", "tie"],
+    ["good", "N", "N"],
+    ["bad", "N", "N"],
+    ["", "tie", "N"],
+    ["good", "tie", "tie"],
+    ["bad", "tie", "tie"]
+]
+
+OBL_TRANSLATION = {
+    # no layer flips
+    "solved/solved": ["solved/solved"],
+
+    "1c/1c": ["1c/1c"],
+    "cadj/cadj": ["cadj/cadj"],
+    "cadj/copp": ["cadj/copp"],
+    "copp/copp": ["copp/copp"],
+    "3c/3c": ["3c/3c"],
+    "4e/4e": ["4e/4e"],
+    "3e/3e": ["3e/3e"],
+    "line/line": ["line/line"],
+    "L/line": ["L/line"],
+    "L/L": ["L/L"],
+    "1e/1e": ["1e/1e"],
+    "good pair/pair": ["left pair/left pair", "right pair/right pair"],
+    "bad pair/pair": ["left pair/right pair"],
+    "good arrow/pair": ["left arrow/right pair", "right arrow/left pair"],
+    "bad arrow/pair": ["left arrow/left pair", "right arrow/right pair"],
+    "good arrow/arrow": ["left arrow/left arrow", "right arrow/right arrow"],
+    "bad arrow/arrow": ["left arrow/right arrow"],
+    "gem/gem": ["gem/gem"],
+    "gem/knight": ["gem/left knight", "gem/right knight"],
+    "gem/axe": ["gem/left axe", "gem/right axe"],
+    "gem/squid": ["gem/squid"],
+    "good knight/knight": ["left knight/right knight"],
+    "bad knight/knight": ["left knight/left knight", "right knight/right knight"],
+    "good knight/axe": ["left knight/left axe", "right knight/right axe"],
+    "bad knight/axe": ["left knight/right axe", "right knight/left axe"],
+    "same axe/axe": ["left axe/left axe", "right axe/right axe"],
+    "diff axe/axe": ["left axe/right axe"],
+    "squid/knight": ["squid/left knight", "squid/right knight"],
+    "squid/axe": ["squid/left axe", "squid/right axe"],
+    "squid/squid": ["squid/squid"],
+    "good thumb/thumb": ["left thumb/left thumb", "right thumb/right thumb"],
+    "bad thumb/thumb": ["left thumb/right thumb"],
+    "good thumb/bunny": ["left thumb/right bunny", "right thumb/left bunny"],
+    "bad thumb/bunny": ["left thumb/left bunny", "right thumb/right bunny"],
+    "good bunny/bunny": ["left bunny/left bunny", "right bunny/right bunny"],
+    "bad bunny/bunny": ["left bunny/right bunny"],
+    "shell/shell": ["shell/shell"],
+    "shell/bird": ["shell/left bird", "shell/right bird"],
+    "shell/hazard": ["shell/hazard"],
+    "yoshi/shell": ["left yoshi/shell", "right yoshi/shell"],
+    "good bird/bird": ["left bird/right bird"],
+    "bad bird/bird": ["left bird/left bird", "right bird/right bird"],
+    "bird/hazard": ["left bird/hazard", "right bird/hazard"],
+    "hazard/hazard": ["hazard/hazard"],
+    "good yoshi/bird": ["left yoshi/left bird", "right yoshi/right bird"],
+    "bad yoshi/bird": ["left yoshi/right bird", "right yoshi/left bird"],
+    "yoshi/hazard": ["left yoshi/hazard", "right yoshi/hazard"],
+    "same yoshi/yoshi": ["left yoshi/left yoshi", "right yoshi/right yoshi"],
+    "diff yoshi/yoshi": ["left yoshi/right yoshi"],
+    "good kite/kite": ["left kite/left kite", "right kite/right kite"],
+    "bad kite/kite": ["left kite/right kite"],
+    "good kite/cut": ["left kite/left cut", "right kite/right cut"],
+    "bad kite/cut": ["left kite/right cut", "right kite/left cut"],
+    "kite/T": ["left kite/black T", "left kite/white T",
+                "right kite/black T", "right kite/white T"],
+    "good kite/N": ["left kite/right N", "right kite/left N"],
+    "bad kite/N": ["left kite/left N", "right kite/right N"],
+    "kite/tie": ["left kite/black tie", "left kite/white tie",
+                "right kite/black tie", "right kite/white tie"],
+    "cut/T": ["left cut/black T", "left cut/white T",
+                "right cut/black T", "right cut/white T"],
+    "good cut/N": ["left cut/left N", "right cut/right N"],
+    "bad cut/N": ["left cut/right N", "right cut/left N"],
+    "cut/tie": ["left cut/black tie", "left cut/white tie",
+                "right cut/black tie", "right cut/white tie"],
+    "good cut/cut": ["left cut/left cut", "right cut/right cut"],
+    "bad cut/cut": ["left cut/right cut"],
+    "good T/T": ["black T/black T", "white T/white T"],
+    "bad T/T": ["black T/white T"],
+    "T/N": ["black T/left N", "black T/right N",
+            "white T/left N", "white T/right N"],
+    "good T/tie": ["black T/black tie", "white T/white tie"],
+    "bad T/tie": ["black T/white tie", "white T/black tie"],
+    "good N/N": ["left N/left N", "right N/right N"],
+    "bad N/N": ["left N/right N"],
+    "tie/N": ["black tie/left N", "black tie/right N",
+            "white tie/left N", "white tie/right N"],
+    "good tie/tie": ["black tie/black tie", "white tie/white tie"],
+    "bad tie/tie": ["black tie/white tie"]
+}
+
+OBL_LEN = {
+    "solved/solved": 0,
+
+    "1c/1c": 5,
+    "cadj/cadj": 4,
+    "cadj/copp": 5,
+    "copp/copp": 2,
+    "3c/3c": 5,
+    "4e/4e": 4,
+    "3e/3e": 5,
+    "line/line": 2,
+    "L/line": 5,
+    "L/L": 4,
+    "1e/1e": 5,
+    "good pair/pair": 2,
+    "bad pair/pair": 4,
+    "good arrow/pair": 3,
+    "bad arrow/pair": 4,
+    "good arrow/arrow": 3,
+    "bad arrow/arrow": 4,
+    "gem/gem": 4,
+    "gem/knight": 4,
+    "gem/axe": 3,
+    "gem/squid": 4,
+    "good knight/knight": 4,
+    "bad knight/knight": 5,
+    "good knight/axe": 3,
+    "bad knight/axe": 4,
+    "same axe/axe": 5,
+    "diff axe/axe": 5,
+    "squid/knight": 4,
+    "squid/axe": 4,
+    "squid/squid": 5,
+    "good thumb/thumb": 2,
+    "bad thumb/thumb": 5,
+    "good thumb/bunny": 4,
+    "bad thumb/bunny": 4,
+    "good bunny/bunny": 3,
+    "bad bunny/bunny": 5,
+    "shell/shell": 4,
+    "shell/bird": 4,
+    "shell/hazard": 4,
+    "yoshi/shell": 3,
+    "good bird/bird": 4,
+    "bad bird/bird": 5,
+    "bird/hazard": 4,
+    "hazard/hazard": 5,
+    "good yoshi/bird": 3,
+    "bad yoshi/bird": 4,
+    "yoshi/hazard": 4,
+    "same yoshi/yoshi": 5,
+    "diff yoshi/yoshi": 5,
+    "good kite/kite": 1,
+    "bad kite/kite": 5,
+    "good kite/cut": 3,
+    "bad kite/cut": 6,
+    "kite/T": 4,
+    "good kite/N": 3,
+    "bad kite/N": 4,
+    "kite/tie": 4,
+    "cut/T": 4,
+    "good cut/N": 4,
+    "bad cut/N": 5,
+    "cut/tie": 4,
+    "good cut/cut": 3,
+    "bad cut/cut": 6,
+    "good T/T": 3,
+    "bad T/T": 4,
+    "T/N": 5,
+    "good T/tie": 3,
+    "bad T/tie": 4,
+    "good N/N": 2,
+    "bad N/N": 4,
+    "tie/N": 5,
+    "good tie/tie": 3,
+    "bad tie/tie":  4
+}
+
+def obl_name(obl: list[str]) -> str:
+    """returns the name in english of this array
+
+    Args:
+        obl (list[str]): e.g. ["", "1c", "1c"]
+
+    Returns:
+        str: e.g. "1c/1c"
+    """
+    return f"{obl[0]} {obl[1]}/{obl[2]}" if obl[0] else f"{obl[1]}/{obl[2]}"
+
+def get_non_spe(obl: str) -> str:
+    """get the non specific case name of an OBL
+
+    Args:
+        obl (str): specific case name, e.g. left pair/right pair
+
+    Returns:
+        str: e.g. good pair/pair
+    """
+    obl_lst = obl.split("/") # len = 2
+    u_obl = obl_lst[0].split(" ")[-1]
+    d_obl = obl_lst[1].split(" ")[-1]
+    candidates = [] # filled with obl arrays of 3
+    for cand in POSSIBLE_OBL:
+        if u_obl in cand and d_obl in cand:
+            candidates.append(obl_name(cand))
+    for cand in candidates:
+        for spe_obl in OBL_TRANSLATION[cand]:
+            if spe_obl == obl:
+                return cand
+            spe_obl = spe_obl.split("/")
+            if spe_obl[1]+"/"+spe_obl[0] == obl:
+                return layer_flip_name(cand) # layer flip
+    raise ValueError("no non-specific OBL found for: "+obl)
+
+def layer_flip_name(obl: str) -> str:
+    """flips an obl name in english
+
+    Args:
+        obl (str): e.g. "good pair/arrow" or "L/line"
+
+    Returns:
+        str: e.g. "good arrow/pair" or "line/L"
+    """
+    obl = obl.replace("/", " ")
+    obla = obl.split(" ")
+    if len(obla) == 2:
+        return obla[1] + "/" + obla[0]
+    return obla[0] + " " + obla[2] + "/" + obla[1]
+
+OBL = {"solved": "BBbBBbBBbBBb",
+
+       "1c": "BBwWWwWWwWWw",
        "cadj": "BBwBBwWWwWWw",
        "copp": "BBwWWwBBwWWw",
        "3c": "BBwBBwBBwWWw",
@@ -45,17 +339,17 @@ OBL = {"1c": "BBwWWwWWwWWw",
        "left yoshi": "BBbWWwBBwWWw",
        "right yoshi": "WWwBBwWWbBBw"
 }
-# format is 24-character string, both corner first
+# format is 24-character string, both corner first, STARTING FROM TOP RIGHT OF SLICE
 CUBEL = 24
 HALF_L = 6
 LAYERL = 12
 THREE_FOUR_L = 18
-SOLVED_a = "BBbBBbBBbBBbWWwWWwWWwWWw"
-SOLVED_A = "bBBbBBbBBbBBwWWwWWwWWwWW"
-SLICE_a_PBL = "FF6GG7DD4AA1BB2CC3HH8EE5"
-SLICE_a = "WWwWWwBBbBBbBBbBBbWWwWWw"
-SLICE_A = "wWWwWWbBBbBBbBBbBBwWWwWW"
-SLICE_A_PBL = "5FF6GG3DD4AA1BB2CC7HH8EE"
+SOLVED_D = "BBbBBbBBbBBbWWwWWwWWwWWw"
+SOLVED_U = "bBBbBBbBBbBBwWWwWWwWWwWW"
+SLICE_D_PBL = "FF6GG7DD4AA1BB2CC3HH8EE5"
+SLICE_D = "WWwWWwBBbBBbBBbBBbWWwWWw"
+SLICE_U = "wWWwWWbBBbBBbBBbBBwWWwWW"
+SLICE_U_PBL = "5FF6GG3DD4AA1BB2CC7HH8EE"
 KARN = {
     "3,0":"U",
     "-3,0":"U'",
@@ -83,7 +377,6 @@ A_MOVES = [[3,0], [-3,0], [0,3], [0,-3], [3,3],
     [2,-1], [-1,2], [-4,-1], [-1,-4], [2,-4], [2,2], [-1,-1], [5,-1]]
 a_MOVES = [[3,0], [-3,0], [0,3], [0,-3], [3,3],
     [-2,1], [1,-2], [4,1], [1,4], [-2,4], [-2,-2], [1,1], [-5,1]]
-# TODO: add more moves?
 KARNL = len(a_MOVES)
 HIGHKARN = {
     # add spaces for de-ambiguity
@@ -248,7 +541,25 @@ def replace_with_dict(s: str, d: dict) -> str:
         s = pattern.sub(lambda m: d[m.group(0)], s)
     return s
 
-def isOBL(layer: str, obl: str) -> bool:
+def do_moves(ms: str, s: str = "BBbBBbBBbBBbwWWwWWwWWwWW") -> str:
+    """does the moves on a cube
+
+    Args:
+        m (str): moves to do. need to start exactly at the cube state
+        s (str, optional): initial cube state. Defaults to a no misalign solved cube.
+        
+    Returns:
+        str: the cube state afterwards
+    """
+    ml = ms.split("/")
+    for m in ml:
+        if m != "":
+            [u, d] = m.split(",")
+            s = move(s, int(u), int(d))
+        s = do_slice(s)
+    return do_slice(s) # undo the last slice
+
+def is_obl(layer: str, obl: str) -> bool:
     """determines if the layer is an obl
 
     Args:
@@ -273,7 +584,7 @@ def isOBL(layer: str, obl: str) -> bool:
                 return True
     return False
 
-def isPBL(layer: str, target: str) -> bool:
+def is_pbl(layer: str, target: str) -> bool:
     """determinesif the layer is the end pbl
 
     Args:
@@ -287,17 +598,17 @@ def isPBL(layer: str, target: str) -> bool:
     if layer[0].isdigit():
         layer = shift(layer,-1)
     for i in range(4):
-        layer = PBL_shift(layer)
+        layer = pbl_shift(layer)
         for m in range(4):
             if target == shift(layer, 3*m):
                 return True
     return False
 
-def randAMove():
+def rand_tm_move():
     """return element of A_MOVES"""
     return list(A_MOVES)[random.randint(0,KARNL-1)]
 
-def randaMove():
+def rand_bm_move():
     """return element of a_MOVES"""
     return list(a_MOVES)[random.randint(0,KARNL-1)]
 
@@ -312,16 +623,17 @@ def layer_flip(state):
     """
     return_val = []
     for c in list(state):
-        if c == "b":
-            return_val.append("w")
-        elif c == "B":
-            return_val.append("W")
-        elif c == "w":
-            return_val.append("b")
-        elif c == "W":
-            return_val.append("B")
-        else:
-            print(c, ": from: layer_flip(): unrecognized piece")
+        match c:
+            case "b":
+                return_val.append("w")
+            case "B":
+                return_val.append("W")
+            case "w":
+                return_val.append("b")
+            case "W":
+                return_val.append("B")
+            case _:
+                print(c, ": from: layer_flip(): unrecognized piece")
     return "".join(return_val)
 
 def shift(a: str, amount: int) -> str:
@@ -340,7 +652,7 @@ def shift(a: str, amount: int) -> str:
         amount += len(a)
     return a[amount:] + a[0:amount]
 
-def PBL_shift(a: str) -> str:
+def pbl_shift(a: str) -> str:
     """shift a PBL layer into one of the four other colors
 
     Args:
@@ -353,12 +665,12 @@ def PBL_shift(a: str) -> str:
     ret = []
     for char in lst:
         if char.isdigit():
-            ret.append(str(PBL_mini_shift(int(char))))
+            ret.append(str(pbl_mini_shift(int(char))))
         else:
-            ret.append(chr(PBL_mini_shift(ord(char) - ord("A") + 1)-1 + ord("A")))
+            ret.append(chr(pbl_mini_shift(ord(char) - ord("A") + 1)-1 + ord("A")))
     return "".join(ret)
 
-def PBL_mini_shift(n: int) -> int:
+def pbl_mini_shift(n: int) -> int:
     """perform a 1-4 and a 5-8 mapping on one number
 
     Args:
@@ -392,7 +704,7 @@ def move(cube: str, u: int, d: int) -> str:
     return (shift(cube[0:LAYERL], u) +
             shift(cube[LAYERL:], d))
 
-def slice(cube: str) -> str:
+def do_slice(cube: str) -> str:
     """does a slice on the cube
 
     Args:
@@ -405,6 +717,25 @@ def slice(cube: str) -> str:
             cube[HALF_L: LAYERL] +
             cube[0: HALF_L] +
             cube[THREE_FOUR_L: CUBEL])
+
+def inverse(ms: str) -> str:
+    """inverses a sequence of squan moves
+
+    Args:
+        m (str): e.g. "/3,0/1,2/2,0"
+
+    Returns:
+        str: e.g. "-2,0/-1,-2/-3,0/"
+    """
+    ml = ms.split("/")
+    ret = []
+    for m in ml:
+        if m == "":
+            ret.insert(0, m)
+        else:
+            [u,d] = m.split(",")
+            ret.insert(0, f"{-1*int(u)},{-1*int(d)}")
+    return "/".join(ret)
 
 def changes_alignment(m: int) -> bool:
     """determines if the given move changes alignment
@@ -543,25 +874,25 @@ def get_scramble(end_state: str, obl: bool, invert_l: bool = True, max_slice: in
     moves = ""
     if obl:
         [u, d] = end_state.split("/") # u: "left gem" and d: "knight"
-        funct = isOBL
+        funct = is_obl
     else:
         [u, d] = [end_state[0:LAYERL], end_state[LAYERL:]]
-        funct = isPBL
+        funct = is_pbl
     while True:
         if random.randint(0,1) == 0:
             # A start
             moves += "A/"
             top_a = True # bool: top misalign?
-            state = SLICE_A if obl else SLICE_A_PBL
+            state = SLICE_U if obl else SLICE_U_PBL
         else:
             # a start
             moves += "a/"
             top_a = False
-            state = SLICE_a if obl else SLICE_a_PBL
+            state = SLICE_D if obl else SLICE_D_PBL
 
         for i in range(max_slice-1):
-            abf = randAMove() if top_a else randaMove()
-            state = slice(move(state, abf[0], abf[1]))
+            abf = rand_tm_move() if top_a else rand_bm_move()
+            state = do_slice(move(state, abf[0], abf[1]))
             moves += f"{abf[0]},{abf[1]}/"
             if changes_alignment(abf[0]):
                 top_a = not top_a
